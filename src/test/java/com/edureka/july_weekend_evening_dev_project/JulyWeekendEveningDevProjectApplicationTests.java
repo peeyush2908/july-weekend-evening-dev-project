8a6372266419721604ab722937a2486b64236f59
Peeyush Yadav <peeyush2908@gmail.com>
2025-11-02 13:20:20 +0530

    initial commit

---
+++ src/test/java/com/edureka/july_weekend_evening_dev_project/JulyWeekendEveningDevProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edureka.july_weekend_evening_dev_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JulyWeekendEveningDevProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
